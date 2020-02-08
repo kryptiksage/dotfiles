6b0bfcb13b288a32777411d9da52edc4f8ea7022
kryptiksage <kryptiksage@gmail.com>
2020-02-08 23:44:59 +0530

    install.sh : Basic setup

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sym=( .vimrc .Xresources .zshrc .fonts .config/{i3,polybar,rofi} .p10k.zsh )
+[ ! -d $HOME/.config ] && mkdir $HOME/.config
+
+# Symlink after deleting existing
+for i in "${sym[@]}"
+do
+	[ -e $HOME/$i ] && rm -rf $HOME/$i
+	ln -s $(pwd)/$i $HOME/$i
+done
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k 2>/dev/null
+
+# zsh
+sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting
+source $HOME/.zshrc 2>/dev/null
+
+# Apply .Xresources
+xrdb .Xresources
