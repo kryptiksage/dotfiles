7dfa11a48fdaf96b69e514c261e94b28d35e1d4d
kryptiksage <kryptiksage@gmail.com>
2020-04-19 18:47:44 +0530

    setup.sh : added branch depth=1

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #/bin/sh/
-git clone https://github.com/kryptiksage/dotfiles.git
+git clone --branch=1 https://github.com/kryptiksage/dotfiles.git
 cd dotfiles
 echo "Exec permission for install.sh"
 chmod +x install.sh
