6a8a8382a0560982e224c0ffac1ce344827ce6e8
Harikrishnan A <47594578+kryptiksage@users.noreply.github.com>
2020-04-19 18:53:29 +0530

    setup.sh : fixed stupid mistake

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #/bin/sh/
-git clone --branch=1 https://github.com/kryptiksage/dotfiles.git
+git clone --depth=1 https://github.com/kryptiksage/dotfiles.git
 cd dotfiles
 echo "Exec permission for install.sh"
 chmod +x install.sh
