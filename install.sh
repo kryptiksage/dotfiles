198ee272e3231c250e2586032bd27a2a01c22ec6
kryptiksage <kryptiksage@gmail.com>
2020-08-05 18:47:40 +0530

    install.sh : reverted to x11 from wayland; replaced st woth alacritty

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-packages=(zsh xf86-video-intel picom reflector zsh-autosuggestions zsh-syntax-highlighting firefox code bluez bluez-utils pulseaudio-bluetooth zsh-theme-powerlevel10k nodejs yarn rustup rust-analyzer-bin telegram-desktop qbittorrent tree alacritty dmenu sway waybar)
+packages=(zsh xf86-video-intel i3-gaps i3blocks i3lock i3status dmenu rofi picom reflector zsh-autosuggestions zsh-syntax-highlighting firefox code bluez bluez-utils pulseaudio-bluetooth zsh-theme-powerlevel10k mupdf nodejs yarn rustup rust-analyzer-bin feh lxappearance telegram-desktop qbittorrent tree alacritty)
 
 DOT_DIR=$(pwd)
 
@@ -18,7 +18,7 @@ echo "Switching to zsh"
 [ $(echo $SHELL) != /bin/zsh ] && chsh -s /bin/zsh
 echo "Changed shell to zsh"
 
-sym=( .zshrc .fonts .config/{mpv,nvim,sway} .p10k.zsh )
+sym=( .Xresources .zshrc .fonts .config/{i3,polybar,rofi,mpv,nvim,alacritty} .p10k.zsh )
 [ ! -d $HOME/.config ] && mkdir $HOME/.config
 
 # Symlink after deleting existing
@@ -36,8 +36,37 @@ nvim +PlugInstall +"call coc#util#install()" +q +q
 cd $HOME/.config/nvim/plugged/coc.nvim ; yarn install
 nvim +"CocInstall coc-rust-analyzer" +q +q
 
+# Bluetooth
+chmod +x $HOME/.config/i3/bluet.sh
+
+# Switch default displays
+cp .xprofile $HOME/.xprofile
+echo -n "Switch to HDMI (y/n) : " 
+read display
+while true
+do
+	case $display in
+		y | Y)
+		echo export DISP_VAR='"$(xrandr --output eDP1 --off --output HDMI1 --primary)"' > $HOME/.xprofile
+		echo Display set to HDMI
+		break
+		;;
+		n | N)
+		echo Display set to default
+		break
+		;;
+	esac
+done
+source $HOME/.xprofile
+
 # Wallpaper
 mkdir $HOME/Pictures
 cp $(pwd)/wallpaper.jpg $HOME/Pictures/
+
+# Apply .Xresources
+xrdb .Xresources
+
+# i3
+i3-msg restart
 
 source $HOME/.zshrc 2>/dev/null
