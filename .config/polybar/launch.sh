1a3bf63345ecde052c94d6fede7dbb2420524d98
kryptiksage <kryptiksage@gmail.com>
2020-09-05 18:38:59 +0530

    .config/polybar/ : changed polybar style

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # network
 export WLAN=$(ip link | grep wlp | awk -F ": " '{print $2}')
 export ETH=$(ip link | grep enp | awk -F ": " '{print $2}')
 
-killall -qw polybar
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
-# Launch bars top and bottom
-echo "---" | tee -a /tmp/polybar_top.log /tmp/polybar_bottom.log
-#polybar top >>/tmp/polybar_top.log 2>&1 &
-polybar bottom >>/tmp/polybar_bottom.log 2>&1 &
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar left >>/tmp/polybar1.log 2>&1 &
+polybar center >>/tmp/polybar2.log 2>&1 &
+polybar whiteq >>/tmp/polybar3.log 2>&1 &
+polybar launcher >>/tmp/polybar1.log 2>&1 &
+polybar right >>/tmp/polybar1.log 2>&1 &
+
 
 echo "Bars launched..."
+
