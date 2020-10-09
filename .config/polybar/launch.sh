9ae17c08d9d2c7159340e8017733ecf073c237d4
kryptiksage <kryptiksage@gmail.com>
2020-10-09 19:45:50 +0530

    .config/polybar : changed to new polybar theme

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,13 +10,7 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar left >>/tmp/polybar1.log 2>&1 &
-polybar center >>/tmp/polybar2.log 2>&1 &
-polybar whiteq >>/tmp/polybar3.log 2>&1 &
-polybar launcher >>/tmp/polybar1.log 2>&1 &
-polybar right >>/tmp/polybar1.log 2>&1 &
-
+echo "---" | tee -a /tmp/bottom.log
+polybar bottom >>/tmp/bottom.log 2>&1 &
 
 echo "Bars launched..."
-
