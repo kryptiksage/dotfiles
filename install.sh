91c4761b38255323f236b79d63130acaae12f426
kryptiksage <kryptiksage@gmail.com>
2021-07-04 18:33:45 +0000

    install.sh : added slurp to package list

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Package List
-packages=(alacritty bluez bluez-utils firefox grim kdeconnect pulseaudio-bluetooth qbittorrent reflector sway telegram-desktop waybar wdisplays zsh zsh-autosuggestions zsh-completions zsh-theme-powerlevel10k)
+packages=(alacritty bluez bluez-utils firefox grim kdeconnect pulseaudio-bluetooth qbittorrent reflector slurp sway telegram-desktop waybar wdisplays zsh zsh-autosuggestions zsh-completions zsh-theme-powerlevel10k)
 
 # Package Install
 for i in "${packages[@]}"
