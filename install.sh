671209ea7e7d9076843480834b3b728ea0a3af73
kryptiksage <kryptiksage@gmail.com>
2021-07-06 19:55:51 +0530

    install.sh : added colors to messages; added addititonal messages

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
+# Colors
+NOCOLOR='\033[0m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+
 # Package List
 packages=(alacritty bat bluez bluez-utils exa firefox grim kdeconnect nodejs pulseaudio-bluetooth qbittorrent reflector rustup rust-analyzer slurp sway telegram-desktop waybar wdisplays yarn zsh zsh-autosuggestions zsh-completions zsh-theme-powerlevel10k)
 
 # Package Install
-echo "Installing packages"
+echo "${ORANGE}Installing packages${NOCOLOR}"
 for i in "${packages[@]}"
 do
 	pacman -Qq $i 2>/dev/null || sudo pacman -S --noconfirm $i 
 done
+echo "${GREEN}Installed${NOCOLOR}"
 
 # Zsh
-echo "Changing Shell"
+echo "${ORANGE}Changing Shell${NOCOLOR}"
 [[ "$SHELL" == *"zsh"* ]] || chsh -s /bin/zsh
-echo "Shell changed to zsh"
+echo "${GREEN}Shell changed to zsh${NOCOLOR}"
 
 # Symlink 
-echo "Symlinking dotfiles"
+echo "${ORANGE}Symlinking dotfiles${NOCOLOR}"
 sym=(.config/{alacritty,nvim,sway,waybar} .fonts .p10k.zsh .zshrc)
 for i in "${sym[@]}"
 do
@@ -26,19 +32,23 @@ do
 		ln -s $PWD/$i $HOME/$i
 	fi
 done
+echo "${GREEN}Symlink done${NOCOLOR}"
 
 # Wallpaper
-echo "Changing wallpaper"
+echo "${ORANGE}Changing wallpaper${NOCOLOR}"
 [ ! -e ~/Pictures/wallpaper.jpg ] || rm -rf ~/Pictures/wallpaper.jpg
 [ -e ~/Pictures ] || mkdir ~/Pictures
 ln -s $PWD/wallpaper.jpg ~/Pictures/wallpaper.jpg
+echo "${GREEN}Default wallpaper linked${NOCOLOR}"
 
 # Rust
-echo "Installing rust stable toolchain"
+echo "${ORANGE}Installing rust stable toolchain${NOCOLOR}"
 rustup toolchain install stable
+echo "${GREEN}Rust installed${NOCOLOR}"
 
 # Time
-echo "Setting timezone/time"
+echo "${ORANGE}Setting timezone/time${NOCOLOR}"
 sudo systemctl --now enable systemd-timesyncd.service
 timedatectl set-timezone Asia/Kolkata
 sudo hwclock --systohc
+echo "${GREEN}Timezone set${NOCOLOR}"
