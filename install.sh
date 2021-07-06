8cd37351d8e7932f84af6bc184f4cb0517960a91
kryptiksage <kryptiksage@gmail.com>
2021-07-06 17:05:38 +0530

    install.sh : added messages to show steps being executed

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 packages=(alacritty bat bluez bluez-utils exa firefox grim kdeconnect nodejs pulseaudio-bluetooth qbittorrent reflector rustup rust-analyzer slurp sway telegram-desktop waybar wdisplays yarn zsh zsh-autosuggestions zsh-completions zsh-theme-powerlevel10k)
 
 # Package Install
+echo "Installing packages"
 for i in "${packages[@]}"
 do
 	pacman -Qq $i 2>/dev/null || sudo pacman -S --noconfirm $i 
@@ -15,6 +16,7 @@ echo "Changing Shell"
 echo "Shell changed to zsh"
 
 # Symlink 
+echo "Symlinking dotfiles"
 sym=(.config/{alacritty,nvim,sway,waybar} .fonts .p10k.zsh .zshrc)
 for i in "${sym[@]}"
 do
@@ -26,14 +28,17 @@ do
 done
 
 # Wallpaper
+echo "Changing wallpaper"
 [ ! -e ~/Pictures/wallpaper.jpg ] || rm -rf ~/Pictures/wallpaper.jpg
 [ -e ~/Pictures ] || mkdir ~/Pictures
 ln -s $PWD/wallpaper.jpg ~/Pictures/wallpaper.jpg
 
 # Rust
+echo "Installing rust stable toolchain"
 rustup toolchain install stable
 
 # Time
+echo "Setting timezone/time"
 sudo systemctl --now enable systemd-timesyncd.service
 timedatectl set-timezone Asia/Kolkata
 sudo hwclock --systohc
